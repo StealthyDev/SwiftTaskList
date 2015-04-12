f7dbbcddc73e05c78fe621ad8e1c683b80ef7f4f
Harshavardhan Kathavate <Harsh@Harshavardhans-MacBook-Air.local>
2015-04-12 14:43:12 -0400

    initial commit
    Signed-off-by: Harshavardhan Kathavate <Harsh@Harshavardhans-MacBook-Air.local>

--- My Task List/SecondViewController.swift
+++ My Task List/SecondViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var txtTask : UITextField!
+    @IBOutlet var txtDesc : UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,25 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Events
+    @IBAction func btnAddTask_Click(sender:UIButton) {
+        taskMgr.addTask(txtTask.text, desc: txtDesc.text)
+        self.view.endEditing(true)
+        txtTask.text = ""
+        txtDesc.text = ""
+        self.tabBarController?.selectedIndex = 0
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        //self.view.endEditing(true)
+    }
+    
+    // UITextField Deligate
+    func textFieldShouldReturn(textField: UITextField) -> Bool  {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
